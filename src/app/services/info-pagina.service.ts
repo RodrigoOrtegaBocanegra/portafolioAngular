91f6db30f2205ca84dd97e9a236576f0e20a3fd9
RodrigoOrtegaBocanegra <a20180289@pucp.edu.pe>
2022-03-14 11:04:17 -0500

    Fin de la seccion 6

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  equipo: any[] = [];
+
+  constructor(private http: HttpClient) { 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+
+  private cargarInfo(){
+    //Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json').subscribe((resp: InfoPagina) => {
+      console.log(resp);
+      this.cargada = true;
+      this.info = resp;
+    });
+  }
+
+  private cargarEquipo(){
+    //Leer el archivo JSON
+    this.http.get('https://angular-html-ea99e-default-rtdb.firebaseio.com/equipo.json').subscribe( (resp: any) => {
+      // console.log(resp);
+      this.equipo = resp;
+    });
+
+
+  }
+
+}
